ddc313677568209fd655d5444d95155f5dcd8a08
BartekCzesak <225974@student.pwr.edu.pl>
2016-05-25 08:36:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <queue>
+#include "Base.h"
+#include <fstream>
+#include <iterator>
+#include <string>
+
 
 using namespace std;
 
-main(int argc,char* argv[])
+//void adapters(Room *, int);
+
+//void adapters(Room * colonists, int n)
+//{
+//	queue <Room *> Corridors;
+//
+//	for (int i = 0; i<n; i++)
+//	{
+//		Corridors.push(colonists + i);
+//	}
+//}
+
+int main(int argc, char* argv[])
 {
-  
-  
+	int n = 10;
+	Colonist *colonists = new Colonist[n];
+	cout << "# Generated Colonists : " << endl;
+	for (int i = 0; i < n; ++i)
+	{
+	   cout << colonists[i].description() << endl;
+    }
+
+	 //adapters(colonists, n);
+
+
+	
+
 }
